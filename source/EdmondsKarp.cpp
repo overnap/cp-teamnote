b9daf9f34b7ca4c255e5d7f2e271c36db82935dd
overnap <overnapworks@gmail.com>
2022-11-17 17:27:31 +0900

    Fix typos

--- source/EdmondsKarp.cpp
+++ source/EdmondsKarp.cpp
@@ -1,11 +1,10 @@
 while (true) {
     vector<int> prev(n, -1);
     queue<int> q;
-    q.push(0);
-    while (!q.empty() && prev[n-1] == -1) {
+    q.push(start);
+    while (!q.empty() && prev[end] == -1) {
         const int x = q.front();
         q.pop();
-
         for (int next : e[x]) {
             if (cap[x][next] - flow[x][next] > 0 && prev[next] == -1) {
                 prev[next] = x;
@@ -13,12 +12,12 @@ while (true) {
             }
         }
     }
-    if (prev[n-1] == -1)
+    if (prev[end] == -1)
         break;
     int bot = 1e9+7;
-    for (int i=n-1; i!=0; i=prev[i])
+    for (int i=end; i!=start; i=prev[i])
         bot = min(bot, cap[prev[i]][i] - flow[prev[i]][i]);
-    for (int i=n-1; i!=0; i=prev[i]) {
+    for (int i=end; i!=start; i=prev[i]) {
         flow[prev[i]][i] += bot;
         flow[i][prev[i]] -= bot;
     }
