f68169b82a11645550885d2526459c940429f209
overnap <overnapworks@gmail.com>
2024-04-16 11:41:16 +0900

    Add pbds

--- source/Template.cpp
+++ source/Template.cpp
@@ -1,13 +1,10 @@
-// fastio
-ios::sync_with_stdio(false);
-cin.tie(nullptr);
 // precision
 cout.precision(16);
 cout << fixed;
 // gcc bit operator
-__builtin_popcount(bits) // popcountll for ll
-__builtin_clz(bits) // left
-__builtin_ctz(bits) // right
+__builtin_popcount(bits); // popcountll for ll
+__builtin_clz(bits);      // left
+__builtin_ctz(bits);      // right
 // random number generator
 random_device rd;
 mt19937 mt;
@@ -15,15 +12,24 @@ uniform_int_distribution<> half(0, 1);
 cout << half(mt);
 // 128MB = int * 33,554,432
 struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
-    }
+  static uint64_t splitmix64(uint64_t x) {
+    // http://xorshift.di.unimi.it/splitmix64.c
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM =
+        chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
 };
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename K, typename V, typename Comp = less<K>>
+using ordered_map =
+    tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename Comp = less<K>>
+using ordered_set = ordered_map<K, null_type, Comp>;
