13f1c8d14c4e9f1da7f0287dee32d4c3d3151b9a
Seunghwan Kim <overnapworks@gmail.com>
2024-08-01 02:39:35 +0900

    Add full version of Hopcroft-Karp

--- source/HopcroftKarp.cpp
+++ source/HopcroftKarp.cpp
@@ -1,35 +1,50 @@
-for (int next : e[x]) {
-  if (match[next] != -1 && level[match[next]] == -1) {
-    level[match[next]] = level[x] + 1;
-    q.push(match[next]);
+while (true) {
+  vector<int> level(sz, -1);
+  queue<int> q;
+  for (int x : l) {
+    if (match[x] == -1) {
+      level[x] = 0;
+      q.push(x);
+    }
   }
-} // bfs loop FIXME: 헷갈리니까 풀버전
-if (level.empty() || *max_element(level.begin(), level.end()) == -1)
-  break;
-for (int next : e[x]) {
-  if (match[next] == -1 ||
-      (level[match[next]] == level[x] + 1 && dfs(match[next]))) {
-    match[next] = x;
-    match[x] = next;
-    return true;
+  while (!q.empty()) {
+    const int x = q.front();
+    q.pop();
+    for (int next : e[x]) {
+      if (match[next] != -1 && level[match[next]] == -1) {
+        level[match[next]] = level[x] + 1;
+        q.push(match[next]);
+      }
+    }
   }
-} // dfs loop
-set<int> alt;
+  if (level.empty() || *max_element(level.begin(), level.end()) == -1)
+    break;
+  function<bool(int)> dfs = [&](int x) {
+    for (int next : e[x]) {
+      if (match[next] == -1 ||
+          (level[match[next]] == level[x] + 1 && dfs(match[next]))) {
+        match[next] = x;
+        match[x] = next;
+        return true;
+      }
+    }
+    return false;
+  };
+  int total = 0;
+  for (int x : l) if (level[x] == 0)  total += dfs(x);
+  if (total == 0) break;
+  flow += total;
+}
+set<int> alt; // Konig
 function<void(int, bool)> dfs = [&](int x, bool left) {
-  if (alt.contains(x))
-    return;
+  if (alt.contains(x)) return;
   alt.insert(x);
   for (int next : e[x]) {
-    if ((next != match[x]) && left)
-      dfs(next, false);
-    if ((next == match[x]) && !left)
-      dfs(next, true);
+    if ((next != match[x]) && left) dfs(next, false);
+    if ((next == match[x]) && !left) dfs(next, true);
   }
 };
-for (int x : l) {
-  if (match[x] == -1)
-    dfs(x, true);
-}
+for (int x : l) if (match[x] == -1) dfs(x, true);
 int test = 0;
 for (int i : l) {
   if (alt.contains(i)) {
