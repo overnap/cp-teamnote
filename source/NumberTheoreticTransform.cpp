1b67b353a5d807e690f525e116d834074dfee7ae
Seunghwan Kim <overnapworks@gmail.com>
2024-08-02 07:53:20 +0900

    Update ucpc 2024

--- source/NumberTheoreticTransform.cpp
+++ source/NumberTheoreticTransform.cpp
@@ -15,3 +15,63 @@ void ntt(vector<ll> &f, const ll w, const ll mod) {
 		x = x*w%mod;
 	}
 }
+vector<int> mult(vector<int> f, vector<int> g) {
+  int sz;
+  for (sz = 1; sz < f.size() + g.size(); sz *= 2);
+  vector<int> ret(sz);
+  f.resize(sz), g.resize(sz);
+  int w = modpow(W, (MOD - 1) / sz, MOD);
+  ntt(f, w), ntt(g, w);
+  for (int i = 0; i < sz; ++i)
+    ret[i] = 1LL * f[i] * g[i] % MOD;
+  ntt(ret, modpow(w, MOD - 2, MOD));
+  const int szinv = modpow(sz, MOD - 2, MOD);
+  for (int i = 0; i < sz; ++i)
+    ret[i] = 1LL * ret[i] * szinv % MOD;
+  while (!ret.empty() && ret.back() == 0)
+    ret.pop_back();
+  return ret;
+}
+vector<int> inv(vector<int> f, const int DMOD) {
+  vector<int> ret = {modpow(f[0], MOD - 2, MOD)};
+  for (int i = 1; i < DMOD; i *= 2) {
+    vector<int> tmp(f.begin(), f.begin() + min((int)f.size(), i * 2));
+    tmp = mult(ret, tmp);
+    tmp.resize(i * 2);
+    for (int &x : tmp) x = (MOD - x) % MOD;
+    tmp[0] = (tmp[0] + 2) % MOD;
+    ret = mult(ret, tmp);
+    ret.resize(i * 2);
+  }
+  ret.resize(DMOD);
+  return ret;
+}
+vector<int> div(vector<int> a, vector<int> b) {
+  if (a.size() < b.size()) return {};
+  const int DMOD = a.size() - b.size() + 1;
+  reverse(a.begin(), a.end());
+  reverse(b.begin(), b.end());
+  if (a.size() > DMOD) a.resize(DMOD);
+  if (b.size() > DMOD) b.resize(DMOD);
+  b = inv(b, DMOD);
+  auto res = mult(a, b);
+  res.resize(DMOD);
+  reverse(res.begin(), res.end());
+  while (!res.empty() && res.back() == 0) res.pop_back();
+  return res;
+}
+vector<int> mod(vector<int> &&a, vector<int> b) {
+  auto tmp = mult(div(a, b), b);
+  tmp.resize(a.size());
+  for (int i = 0; i < a.size(); ++i)
+		a[i] = (a[i] - tmp[i] + MOD) % MOD;
+  while (!a.empty() && a.back() == 0) a.pop_back();
+  return a;
+}
+// kitamasa
+vector<int> res = {1}, xn = {0, 1};
+while (n) {
+  if (n & 1) res = mod(mult(res, xn), c);
+  n /= 2;
+  xn = mod(mult(xn, xn), c);
+}
--- source/Template.cpp
+++ source/Template.cpp
@@ -31,5 +31,7 @@ using namespace __gnu_pbds;
 template <typename K, typename V, typename Comp = less<K>>
 using ordered_map =
     tree<K, V, Comp, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename K, typename Comp = less<K>> // less_equal for multiset
+template <typename K, typename Comp = less<K>> // less_equal (MS)
 using ordered_set = ordered_map<K, null_type, Comp>;
+regex re("^first.[0-9a-z]?*+{n}{n,m}");
+regex_match(s, re)
